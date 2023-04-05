920915e774393f102f9a6352788ef032330492b8
EasonOWO <>
2023-04-05 07:22:20 +0000

    ALL CHANGE TO SPRITE!!

--- SpriteTset.py
+++ SpriteTset.py
@@ -1,183 +1,175 @@
 import sys
 import pygame
-import numpy as np
 import math
 import os
 import keyboard
 import color
-import player
 
-# 遊戲初始化 & 創建視窗
-pygame.init()
-pygame.display.set_caption("GameTest")
-
-# the width and height of screen
+# 各種變數
 screen_size = S_W, S_H = (500, 500)
 FPS = 60
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
-BLUE = (0, 0, 255)
+
 Radius = 20
 RIGHT_TOP = (0, 0)
 MIDDLE = (S_W // 2, S_H // 2)
-
+BLACK = (0, 0, 0)
+# 遊戲初始化 & 創建視窗
+pygame.init()
+pygame.display.set_caption("GameTest")
 screen = pygame.display.set_mode(screen_size)
-canvas = pygame.Surface(screen.get_size()).convert()
+
 uiscreen = pygame.Surface((500, 50)).convert()
 background = pygame.Surface(screen.get_size()).convert()
 
 michan = pygame.image.load(os.path.join('img', 'michan 500x500.jpg')).convert()
-arrow = pygame.image.load(os.path.join('img', 'arrow.png')).convert()
-#background = michan
+arrow = pygame.image.load(os.path.join('img', 'Arrow.jpg')).convert()
+# background = michan
 clock = pygame.time.Clock()
+
+dfont = pygame.font.SysFont("Arial", 30)
 
 
 class Player1(pygame.sprite.Sprite):
-    acl = 0.2
-    backacl = 0.05
-    maxv = 2.0
 
     def __init__(self, x, y, velx, vely, color):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface(MIDDLE)
-
-        self.x1 = x
-        self.y1 = y
-        self.velx1 = velx
-        self.vely1 = vely
-        self.color1 = color
-
-        self.image.fill(BLUE)
-        self.image.set_colorkey(BLACK)
-
+        self.image = arrow
         self.rect = self.image.get_rect()
 
-    def move_output(self):
+        self.x = x
+        self.y = y
+        self.velx = velx
+        self.vely = vely
+        self.color = color
+
+        self.acl = int(0.2)
+        self.backacl = int(0.05)
+        self.maxv = int(2.0)
+        self.aclx = int(0)
+        self.acly = int(0)
+
+    def update(self):
         key_pressed = pygame.key.get_pressed()
-        aclx = 0
-        acly = 0
-        if key_pressed[pygame.K_LEFT] and self.x1 > 0:
-            aclx = -acl
 
-        if key_pressed[pygame.K_RIGHT] and self.x1 < 500:
-            aclx = acl
+        #移動設定
+        if key_pressed[pygame.K_LEFT] and self.x > 0:
+            self.aclx = -acl
+        if key_pressed[pygame.K_RIGHT] and self.x < 500:
+            self.aclx = acl
+        if key_pressed[pygame.K_UP] and self.y > 0:
+            self.acly = -acl
+        if key_pressed[pygame.K_DOWN] and self.y < 500:
+            self.acly = acl
 
-        if key_pressed[pygame.K_UP] and self.y1 > 0:
-            acly = -acl
+        if self.aclx ** 2 + self.acly ** 2 > acl * acl:  # 單位向量
+            self.aclx *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
+            self.acly *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
 
-        if key_pressed[pygame.K_DOWN] and self.y1 < 500:
-            acly = acl
+        # -------------------------------------------------------------------------------------
+        self.velx += self.aclx
+        self.vely += self.acly
 
-        if aclx ** 2 + acly ** 2 > acl * acl:  # 單位向量
-            aclx *= abs(acl / (math.sqrt(aclx ** 2 + acly ** 2)))
-            acly *= abs(acl / (math.sqrt(aclx ** 2 + acly ** 2)))
-
-        self.rect.x += self.velx1
-        self.rect.y += self.vely1
-        return aclx, acly
-
-    def move_input(self):
-        aclx, acly = Player2.move_output(self, key_pressed)
-        self.velx1 += aclx
-        self.vely1 += acly
-
-        if self.velx1 != 0:
-            if self.velx1 > 0:
-                self.velx1 -= backacl
+        if self.velx != 0:
+            if self.velx > 0:
+                self.velx -= backacl
             else:
-                self.velx1 += backacl
+                self.velx += backacl
 
-        if self.vely1 != 0:
-            if self.vely1 > 0:
-                self.vely1 -= backacl
+        if self.vely != 0:
+            if self.vely > 0:
+                self.vely -= backacl
             else:
-                self.vely1 += backacl
+                self.vely += backacl
 
-        if (self.velx1 ** 2 + self.vely1 ** 2 > maxv ** 2):
-            self.velx1 *= abs(maxv / (math.sqrt(self.velx1 ** 2 + self.vely1 ** 2)))
-            self.vely1 *= abs(maxv / (math.sqrt(self.velx1 ** 2 + self.vely1 ** 2)))
-        self.x1 += self.velx1
-        self.y1 += self.vely1
-        return (self.x1, self.y1)
+        if self.velx ** 2 + self.vely ** 2 > maxv ** 2:
+            self.velx *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+            self.vely *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+
+        self.x += self.velx
+        self.y += self.vely
+
+        #上色
+        pygame.draw.circle(background, color.blue, (self.x, self.y), Radius, Radius - 1)
 
 
 class Player2(pygame.sprite.Sprite):
-    acl = 0.2
-    backacl = 0.05
-    maxv = 2.0
 
-    def __init__(self,x, y, velx, vely, color):
+    def __init__(self, x, y, velx, vely, color):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface(MIDDLE)
-
-        self.x2 = x
-        self.y2= y
-        self.velx2 = velx
-        self.vely2 = vely
-        self.color2 = color
-
-        self.image.fill(BLUE)
-        self.image.set_colorkey(BLACK)
+        self.image = arrow
         self.rect = self.image.get_rect()
 
-        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.velx = velx
+        self.vely = vely
+        self.color = color
 
-    def move_input(self):
+        self.acl = 0.2
+        self.backacl = 0.05
+        self.maxv = 2.0
+
+        self.aclx = 0
+        self.acly = 0
+
+    def update(self):
+        if(self.x >= 500):
+            self.x -= 495
+        if(self.x <= 0):
+            self.x += 495
+        if(self.y >= 500):
+            self.y -= 495
+        if(self.y <= 0):
+            self.y += 495 
         key_pressed = pygame.key.get_pressed()
-        aclx = 0
-        acly = 0
-        if key_pressed[pygame.K_a] and self.x2 > 0:
-            aclx = -acl
 
-        if key_pressed[pygame.K_d] and self.x2 < 500:
-            aclx = acl
+        if key_pressed[pygame.K_a]:
+            self.aclx = -acl
+        if key_pressed[pygame.K_d]:
+            self.aclx = acl
+        if key_pressed[pygame.K_w]:
+            self.acly = -acl
+        if key_pressed[pygame.K_s]:
+            self.acly = acl
 
-        if key_pressed[pygame.K_w] and self.y2 > 0:
-            acly = -acl
+        if self.aclx ** 2 + self.acly ** 2 > acl ** 2:  # 單位向量
+            self.aclx *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
+            self.acly *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
 
-        if key_pressed[pygame.K_s] and self.y2 < 500:
-            acly = acl
+        # -------------------------------------------------------------------------------------
+        self.velx += self.aclx
+        self.vely += self.acly
 
-        if aclx ** 2 + acly ** 2 > acl * acl:  # 單位向量
-            aclx *= abs(acl / (math.sqrt(aclx ** 2 + acly ** 2)))
-            acly *= abs(acl / (math.sqrt(aclx ** 2 + acly ** 2)))
-
-        self.x2 += self.velx2
-        self.y2 += self.vely2
-        return aclx, acly
-
-    def move_output(self, keys):
-        aclx, acly = Player2.move_input(self, key_pressed)
-        self.velx2 += aclx
-        self.vely2 += acly
-        if self.velx2 != 0:
-            if self.velx2 > 0:
-                self.velx2 -= backacl
+        if self.velx != 0:
+            if self.velx > 0:
+                self.velx -= backacl
             else:
-                self.velx2 += backacl
+                self.velx += backacl
 
-        if self.vely2 != 0:
-            if self.vely2 > 0:
-                self.vely2 -= backacl
+        if self.vely != 0:
+            if self.vely > 0:
+                self.vely -= backacl
             else:
-                self.vely2 += backacl
+                self.vely += backacl
 
-        if self.velx2 ** 2 + self.vely2 ** 2 > maxv ** 2:
-            self.velx2 *= abs(maxv / (math.sqrt(self.velx2 ** 2 + self.vely2 ** 2)))
-            self.vely2 *= abs(maxv / (math.sqrt(self.velx2 ** 2 + self.vely2 ** 2)))
-        self.x2 += self.velx2
-        self.y2 += self.vely2
-        return self.x2, self.y2
+        if self.velx ** 2 + self.vely ** 2 > maxv ** 2:
+            self.velx *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+            self.vely *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+
+        self.x += self.velx
+        self.y += self.vely
+
+        pygame.draw.circle(background, color.red, (self.x, self.y), Radius, Radius - 1)
 
 
 ###########################################################
+Player1 = Player1(0, 50, 0, 0, color.blue)
+Player2 = Player2(0, 50, 0, 0, color.red)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(Player1)
+all_sprites.add(Player2)
 
-###########################################################
-player1 = Player1(0, 0, 0, 0,  color.blue)
-player2 = Player2(0, 0, 0, 0,  color.red)
+
 ###########################################################
 
 
@@ -200,35 +192,34 @@ def color_counting(cv):
 
 
 # -----------------------------------------------------------------------------------
-all_sprites = pygame.sprite.Group()
 
 counting_time = int(0)
 frame_count = int(0)
 R = 20  # the r of circle
-x = int(S_W / 2)  # position x
-y = int(S_H / 2)  # position y
-velx = int(0)
-vely = int(0)
 maxv = int(2)
 red_score, b_score = int(0), int(0)
 acl = 0.2
 backacl = 0.05
+
 time = 0  # time of game
-dfont = pygame.font.SysFont("Arial", 30)
+
+font = pygame.font.SysFont("Arial", 20)
 running = True
 
 # 遊戲迴圈
 while running:
+    if frame_count == 0:
+        background.fill(color.white)
+
     counting_time += 1
     frame_count += 1
     pygame.time.Clock()
     pygame.time.delay(10)
-    uiscreen.blit(background, (0, 0))
+
     key_pressed = pygame.key.get_pressed()
 
     clock.tick(FPS)
-    if frame_count==0:
-        background.fill(color.white)
+
     # 取得輸入
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -236,40 +227,29 @@ while running:
 
     # 更新遊戲
     all_sprites.update()
-    pygame.draw.circle(michan, BLUE, (player1.x1,player1.y1), Radius)
-    pygame.draw.circle(michan, RED, (player2.x2,player2.y2), Radius)
+    x1, y1 = Player1.x, Player1.y
+    x2, y2 = Player2.x, Player2.y
 
     # ColorCount
     if frame_count % 500 == 0:
-        red_score, b_score = color_counting(canvas)
+        red_score, b_score = color_counting(background)
         counting_time = int(0)
-    if (key_pressed[pygame.K_SPACE]):
-        print((b_score))
+    if key_pressed[pygame.K_SPACE]:
+        print(b_score)
         print(f"{type(red_score)}")
 
     # 畫面顯示
-    '''
-    # UI
-    p1_location_font = dfont.render(
-        'blue_loc : {0}, {1}'.format(int(Player1.x1), int(Player1.y1)), 1, color.cyan)
-    p2_location_font = dfont.render(
-        'red_loc : {0}, {1}'.format(int(Player2.x2), int(Player2.y2)), 1, color.cyan)
-                               
-    message = dfont.render('{0},{1},{2},{3}'.format(int(player1.x), int(player1.y),(red_score/100),(b_score/100)), 1, color.cyan)
-    '''
-
-    # text of location
-    '''uiscreen.blit(message, (0, 0))
-    uiscreen.blit(p1_location_font, (0, 0))
-    uiscreen.blit(p2_location_font, (275, 0))
-    '''
 
     if frame_count % 500 != 0:
         screen.blit(background, (0, 0))
     screen.blit(uiscreen, (0, 0))
+    message = font.render('blue_loc: {0},{1} | red_loc: {2},{3}'.format(int(x1), int(y1), int(x2), int(y2)), True, color.cyan)
+    uiscreen.blit(message, (0, 0))
 
-    pygame.display.flip()
+    screen.blit(arrow, MIDDLE)
+
     pygame.display.update()
+    pygame.display.flip()
 
 pygame.quit()
 sys.exit()
--- main.py
+++ main.py
@@ -1,58 +1,87 @@
 import sys
 import pygame
-import numpy as np
-import os
 import math
+import os
 import keyboard
 import color
 
-#the start of pygame
+# 各種變數
+screen_size = S_W, S_H = (500, 550)
+FPS = 60
+
+Radius = 20
+RIGHT_TOP = (0, 0)
+RIGHT_BOTTOM =(0,500)
+MIDDLE = (S_W // 2, S_H // 2)
+BLACK = (0, 0, 0)
+# 遊戲初始化 & 創建視窗
 pygame.init()
+pygame.display.set_caption("GameTest")
+screen = pygame.display.set_mode(screen_size)
 
-#the width and height of screen
-S_W = 500
-S_H = 500
-#michan.convert()
+background = pygame.Surface(screen.get_size()).convert()
+
+michan = pygame.image.load(os.path.join('img', 'michan 500x500.jpg')).convert()
+arrow = pygame.image.load(os.path.join('img', 'Arrow.jpg')).convert()
+circle = pygame.image.load(os.path.join('img','circle.png')).convert()
+background = michan
+clock = pygame.time.Clock()
+
+dfont = pygame.font.SysFont("Arial", 30)
 
 
-#########################################################player
-class player:
+class Player1(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, velx, vely, go_up, go_down, go_left, go_right, color):
+    def __init__(self, x, y, velx, vely, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = circle
+        self.rect = self.image.get_rect()
+
         self.x = x
         self.y = y
         self.velx = velx
         self.vely = vely
-        self.go_up = go_up
-        self.go_down = go_down
-        self.go_left = go_left
-        self.go_right = go_right
         self.color = color
 
-    def move_input(self, keys):
-        aclx = 0
-        acly = 0
-        if keys[self.go_left] and self.x > 0:
-            aclx = -acl
+        self.acl = int(0.2)
+        self.backacl = int(0.05)
+        self.maxv = int(2.0)
+        self.aclx = int(0)
+        self.acly = int(0)
 
-        if keys[self.go_right] and self.x < 500:
-            aclx = acl
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        self.aclx = int(0)
+        self.acly = int(0)
+        self.image = arrow
 
-        if keys[self.go_up] and self.y > 0:
-            acly = -acl
+        #移動設定
+        if (self.x >= 500):
+            self.x -= 495
+        if (self.x <= 0):
+            self.x += 495
+        if (self.y >= 500):
+            self.y -= 495
+        if (self.y <= 0):
+            self.y += 495
 
-        if keys[self.go_down] and self.y < 500:
-            acly = acl
+        if key_pressed[pygame.K_LEFT] and self.x > 0:
+            self.aclx = -acl
+        if key_pressed[pygame.K_RIGHT] and self.x < S_W:
+            self.aclx = acl
+        if key_pressed[pygame.K_UP] and self.y > 0:
+            self.acly = -acl
+        if key_pressed[pygame.K_DOWN] and self.y < S_H:
+            self.acly = acl
 
-        if (aclx**2 + acly**2 > acl**2):  #單位向量
-            aclx *= abs(acl / (math.sqrt(aclx**2 + acly**2)))
-            acly *= abs(acl / (math.sqrt(aclx**2 + acly**2)))
-        return aclx, acly
+        if self.aclx ** 2 + self.acly ** 2 > acl * acl:  # 單位向量
+            self.aclx *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
+            self.acly *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
 
-    def move_output(self, keys):
-        aclx, acly = player.move_input(self, keys)
-        self.velx += aclx
-        self.vely += acly
+        # -------------------------------------------------------------------------------------
+        self.velx += self.aclx
+        self.vely += self.acly
+
         if self.velx != 0:
             if self.velx > 0:
                 self.velx -= backacl
@@ -64,121 +93,172 @@ class player:
                 self.vely -= backacl
             else:
                 self.vely += backacl
-                
-        if self.velx != 0 and (self.x>500 or self.x<0):
-            self.velx *= -2
-        if self.vely != 0 and (self.y>500 or self.y<0):
-            self.vely *= -2
-            
-        if (self.velx**2 + self.vely**2 > maxv**2):
-            self.velx *= abs(maxv / (math.sqrt(self.velx**2 + self.vely**2)))
-            self.vely *= abs(maxv / (math.sqrt(self.velx**2 + self.vely**2)))
+
+        if self.velx ** 2 + self.vely ** 2 > maxv ** 2:
+            self.velx *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+            self.vely *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+
         self.x += self.velx
         self.y += self.vely
-        return (self.x, self.y)
+
+        #上色
+        pygame.draw.circle(background, color.blue, (self.x, self.y), Radius, Radius - 1)
+
+
+class Player2(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, velx, vely, color):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = arrow
+        self.rect = self.image.get_rect()
+
+        self.x = x
+        self.y = y
+        self.velx = velx
+        self.vely = vely
+        self.color = color
+
+        self.acl = 0.2
+        self.backacl = 0.05
+        self.maxv = 2.0
+
+        self.aclx = 0
+        self.acly = 0
+
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        self.aclx = 0
+        self.acly = 0
+
+
+        if key_pressed[pygame.K_a] and self.x > 0:
+            self.aclx = -acl
+        if key_pressed[pygame.K_d] and self.x < S_W:
+            self.aclx = acl
+        if key_pressed[pygame.K_w] and self.y > 0:
+            self.acly = -acl
+        if key_pressed[pygame.K_s] and self.y < S_H:
+            self.acly = acl
+
+        if self.aclx ** 2 + self.acly ** 2 > acl * acl:  # 單位向量
+            self.aclx *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
+            self.acly *= abs(acl / (math.sqrt(self.aclx ** 2 + self.acly ** 2)))
+
+        # -------------------------------------------------------------------------------------
+        self.velx += self.aclx
+        self.vely += self.acly
+
+        if self.velx != 0:
+            if self.velx > 0:
+                self.velx -= backacl
+            else:
+                self.velx += backacl
+
+        if self.vely != 0:
+            if self.vely > 0:
+                self.vely -= backacl
+            else:
+                self.vely += backacl
+
+        if self.velx ** 2 + self.vely ** 2 > maxv ** 2:
+            self.velx *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+            self.vely *= abs(maxv / (math.sqrt(self.velx ** 2 + self.vely ** 2)))
+
+        self.x += self.velx
+        self.y += self.vely
+
+        pygame.draw.circle(background, color.red, (self.x, self.y), Radius, Radius - 1)
 
 
 ###########################################################
-player1 = player(0, 0, 0, 0, keyboard.UP, keyboard.DOWN, keyboard.LEFT, keyboard.RIGHT, color.blue)
-player2 = player(0, 0, 0, 0, keyboard.w, keyboard.s, keyboard.a, keyboard.d, color.red)
+Player1 = Player1(0, 50, 0, 0, color.blue)
+Player2 = Player2(0, 50, 0, 0, color.red)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(Player1)
+all_sprites.add(Player2)
+
+
 ###########################################################
 
-#settings
-winScreen = pygame.display.set_mode((S_W, S_H))
-pygame.display.set_caption("Keyboard Demo")
-canvas = pygame.Surface(winScreen.get_size()).convert()
-uiscreen = pygame.Surface((500, 50)).convert()
-background = pygame.Surface(winScreen.get_size()).convert()
-michan = pygame.image.load(os.path.join('img', 'michan 500x500.jpg')).convert()
-#background = michan
 
-#count the score
+# count the score
 def color_counting(cv):
+    red_area_intcode = 16777215
+    blue_area_intcode = 16711680
     area_value = pygame.surfarray.pixels2d(cv)
     red_ar = int(0)
     blue_ar = int(0)
+    print(area_value)
     for i in range(100):
         for j in range(100):
-            if area_value[i * 5][j * 5] == cv.map_rgb(color.red):
+            if area_value[i * 5][j * 5] == red_area_intcode:
                 red_ar += 1
-            if area_value[i * 5][j * 5] == cv.map_rgb(color.blue):
+            if area_value[i * 5][j * 5] == blue_area_intcode:
                 blue_ar += 1
+
     return red_ar, blue_ar
 
 
+# -----------------------------------------------------------------------------------
+
 counting_time = int(0)
 frame_count = int(0)
-R = 20  #the r of circle
-
+R = 20  # the r of circle
 maxv = int(2)
-red_score, blue_score = int(0), int(0)
+red_score, b_score = int(0), int(0)
 acl = 0.2
 backacl = 0.05
-time = 0  #time of game
-run = True  #run the game or not
-dfont = pygame.font.SysFont("Arial", 20)
-########################RUN##############################
-while run:
-    
-    if frame_count==0:
+
+time = 0  # time of game
+
+font = pygame.font.SysFont("Arial", 20)
+running = True
+
+# 遊戲迴圈
+while running:
+    '''
+    if frame_count == 0:
         background.fill(color.white)
-    
+    '''
     counting_time += 1
     frame_count += 1
     pygame.time.Clock()
     pygame.time.delay(10)
-    uiscreen.blit(background, (0, 0))
+
+    key_pressed = pygame.key.get_pressed()
+
+    clock.tick(FPS)
+
+    # 取得輸入
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            running = False
 
-    #move
-    keyInput = pygame.key.get_pressed()
-    player1.x, player1.y = player1.move_output(keyInput)
-    player2.x, player2.y = player2.move_output(keyInput)
-
-    #ColorCount
+    # 更新遊戲
+    all_sprites.update()
+    x1,y1 = Player1.x,Player1.y
+    x2,y2 = Player2.x,Player2.y
+    # ColorCount
     if frame_count % 500 == 0:
-        red_score, blue_score = color_counting(background)
-        print(red_score,blue_score)
+        red_score, b_score = color_counting(background)
         counting_time = int(0)
-    if (keyInput[pygame.K_SPACE]):
-        print((blue_score))
+    if key_pressed[pygame.K_SPACE]:
+        print(b_score)
         print(f"{type(red_score)}")
-    #Paint
-    pygame.draw.circle(background, player1.color, (player1.x, player1.y), R, R - 1)
-    pygame.draw.circle(background, player2.color, (player2.x, player2.y), R, R - 1)
 
-    #UI
-    p1_location_font = dfont.render('blue_loc : {0}, {1}, {2}'.format(int(player1.x), int(player1.y), round(blue_score/100)), 1, color.cyan)
-    p2_location_font = dfont.render('red_loc : {0}, {1}, {2}'.format(int(player2.x), int(player2.y), round(red_score/100)), 1, color.cyan)
-    '''                            
-    message = dfont.render('{0},{1},{2},{3}'.format(int(player1.x), int(player1.y),(red_score/100),(b_score/100)), 1, color.cyan)
-    '''
-    #text of location
-    '''uiscreen.blit(message, (0, 0))'''
-    uiscreen.blit(p1_location_font, (0, 0))
-    uiscreen.blit(p2_location_font, (250, 0))
-    
+    # 畫面顯示
+
     if frame_count % 500 != 0:
-      winScreen.blit(background,(0,0))
-      
-    winScreen.blit(uiscreen, (0, 0))
-    #winScreen.blit(michan, (x, y))
+        screen.blit(background, (0, 0))
 
+    message_loc = font.render('blue_loc: {0},{1} | red_loc: {2},{3}'.format(int(x1), int(y1), int(x2), int(y2)), True, color.cyan)
+    message_scores = font.render('blue_scores:{0} | red_scores:{1}'.format(int(b_score), int(red_score)), True, color.cyan)
+    screen.blit(message_loc, (0,470))
+    screen.blit(message_scores, RIGHT_TOP)
+
+
+    #screen.blit(arrow,MIDDLE)
     pygame.display.update()
+
 pygame.quit()
 sys.exit()
-##MICHAN DAISUKI
-''' 
-*
-*   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
-* ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
-* ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
-* ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
-* ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
-*  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
-*  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
-*  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
-*           ░     ░ ░      ░  ░
-'''
